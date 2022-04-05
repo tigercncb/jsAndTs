c8f57e82904b68aaca77f96b62b8801c38e978a0
971176 <971176@qq.com>
2022-04-05 21:03:07 +0800

    promise

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# jsAndTs
+## jsAndTs
 js和ts一些知识点
+```1:
+codeRUnner停止执行代码快捷键：Ctrl + Alt + M
+```
--- promis.js
+++ promis.js
@@ -1,66 +1,107 @@
 //async Promise await
-class a {
+/**
+var a=0;
+var ps=new Promise((resolve,reject)=>{
+    console.log("进入了promise");
+    setInterval(function(){
+        a++;
+        console.log("time:"+a)
+        resolve(a)
+    },2000)
+    console.log("这里执行了")
+})
+ps.then((vale)=>{
+    console.log("第几个了："+vale)
+}).catch(
 
-
-    test() {
-
-        console.log("click!!!")
-        let x = this.f1();
-        // this.f2()
-        // this.testPromise();
-        console.log("end asyn!!!!")
+    // 记录失败原因
+    (reason) => {
+        console.log("处理失败" + reason)
     }
-    testPromise() {
-        let lab = this.labs
-        let thisProCount = ++this.promiseCount;
-        lab.text += "同步代码开始!"
+)
+ */
+//-----------------------------------------------------------------------------------------------
+function resolveAfter2Seconds() {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve('resolved');
+      }, 2000);
+    });
+  }
+async function asyncCall() {
+    console.log('calling');
+    const result = await resolveAfter2Seconds();
+    console.log(result);
+    // expected output: "resolved"
+  }
+  asyncCall();
 
-        //新构建一个 Promise 实例：使用Promise实现每过一段时间给计数器加一的过程，每段时间间隔为1~3秒不等
-        let p1 = new Promise((resolve, reject) => {
+//-----------------------------------------------------------------------------------------------
 
-            // resolver 函数在 Promise 成功或失败时都可能被调用
-            lab.text += "异步开始!"
 
-            // 创建一个异步调用
-            window.setTimeout(function () {
 
-                // 填充 Promise
-                resolve(thisProCount)
-            }, Math.random() * 2000 + 1000)
-        })
+// class a {
 
-        // Promise 不论成功或失败都会调用 then
-        // catch() 只有当 promise 失败时才会调用
-        p1.then(
-            function (val) {
-                lab.text += val + "填充完毕，异步结束!"
-            }
-        )
-            .catch(
 
-                // 记录失败原因
-                (reason) => {
-                    console.log("处理失败" + reason)
-                }
-            )
-        lab.text += "同步代码结束!"
-    }
-    /////////////////////////////////////////////////////////////////////////////////////////
-    resolveAfter2Seconds(x) {
-        return new Promise(resolve => {
-            setTimeout(() => {
-                resolve(x);
-            }, 2000);
-        });
-    }
+//     test() {
 
-    async f1() {
-        var x = await this.resolveAfter2Seconds(10);
-        console.log("await result:" + x); // 10
-        return x;
-    }
-    async f2() {
-        var y = await 20;
-        console.log("f2:" + y); // 20
-    }
-}
+//         console.log("click!!!")
+//         let x = this.f1();
+//         // this.f2()
+//         // this.testPromise();
+//         console.log("end asyn!!!!")
+//     }
+//     testPromise() {
+//         let lab = this.labs
+//         let thisProCount = ++this.promiseCount;
+//         lab.text += "同步代码开始!"
+
+//         //新构建一个 Promise 实例：使用Promise实现每过一段时间给计数器加一的过程，每段时间间隔为1~3秒不等
+//         let p1 = new Promise((resolve, reject) => {
+
+//             // resolver 函数在 Promise 成功或失败时都可能被调用
+//             lab.text += "异步开始!"
+
+//             // 创建一个异步调用
+//             window.setTimeout(function () {
+
+//                 // 填充 Promise
+//                 resolve(thisProCount)
+//             }, Math.random() * 2000 + 1000)
+//         })
+
+//         // Promise 不论成功或失败都会调用 then
+//         // catch() 只有当 promise 失败时才会调用
+//         p1.then(
+//             function (val) {
+//                 lab.text += val + "填充完毕，异步结束!"
+//             }
+//         )
+//             .catch(
+
+//                 // 记录失败原因
+//                 (reason) => {
+//                     console.log("处理失败" + reason)
+//                 }
+//             )
+//         lab.text += "同步代码结束!"
+//     }
+//     /////////////////////////////////////////////////////////////////////////////////////////
+//     resolveAfter2Seconds(x) {
+//         return new Promise(resolve => {
+//             setTimeout(() => {
+//                 resolve(x);
+//             }, 2000);
+//         });
+//     }
+
+//     async f1() {
+//         var x = await this.resolveAfter2Seconds(10);
+//         console.log("await result:" + x); // 10
+//         return x;
+//     }
+//     async f2() {
+//         var y = await 20;
+//         console.log("f2:" + y); // 20
+//     }
+// }
